2a0c88ba9140511b92de96d5af43ccebefb84750
AlinBanka <alin.banka@gmail.com>
2018-11-06 19:31:20 +0300

    Add files via upload

---
+++ form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, validators
+
+
+class RegistrationForm(Form):
+    username = StringField('username', [validators.Length(min=4, max=25)])
+    name = StringField('name', [validators.Length(min=6, max=35)])
+    surname = StringField('surname', [validators.Length(min=6, max=35)])
+    password = PasswordField('new password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('retype password')
+    studentno = StringField('student number', [validators.Length(9)])
