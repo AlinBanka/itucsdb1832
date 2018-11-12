c6445ab22e247479b8b6635e165a9427cab63792
AlinBanka <alin.banka@gmail.com>
2018-11-13 01:52:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -11,11 +11,13 @@ statement = """CREATE TABLE IF NOT EXISTS students(
         surname VARCHAR NOT NULL
         )"""
 
-#connection = db.connect("dbname='itucsdb' user='postgres' host='localhost' port=5431 password=''")
-#cursor = connection.cursor()
-#cursor.execute(statement)
-#connection.commit()
-#cursor.close()
+url = "postgres://itucs:itucspw@localhost:32775/itucsdb"
+
+connection = db.connect(url)
+cursor = connection.cursor()
+cursor.execute(statement)
+connection.commit()
+cursor.close()
 
 
 @app.route("/")
@@ -31,11 +33,11 @@ def register():
         name = form.name.data
         surname = form.surname.data
         try:
-            #connection = db.connect("dbname='postgres' user='postgres' host='localhost' password=''")
-            #cursor = connection.cursor()
-            #statement = "INSERT INTO students VALUES(%s, %s, %s, %s)"
-            #cursor.execute(statement, (usename, password, name, surname))
-            #result = cursor.fetchone()
+            connection = db.connect("dbname='postgres' user='postgres' host='localhost' password=''")
+            cursor = connection.cursor()
+            statement = "INSERT INTO students VALUES(%s, %s, %s, %s)"
+            cursor.execute(statement, (usename, password, name, surname))
+            result = cursor.fetchone()
             flash('Your account was created successfully')
         except db.DatabaseError:
             connection.rollback()
