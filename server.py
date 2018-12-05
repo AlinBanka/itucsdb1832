39877ec698082bd1ed445133761d89795b5dba51
AlinBanka <alin.banka@gmail.com>
2018-12-05 14:11:27 +0300

    adding new changes to the server

--- server.py
+++ server.py
@@ -1,54 +1,65 @@
-from flask import Flask, render_template, url_for, flash
-from form import RegistrationForm
+from flask import Flask, render_template, url_for, flash, request, session, redirect
+from form import RegistrationForm, LoginForm
+from dbinit import initialize
 import psycopg2 as db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234w7ecF4gh321q'
 
-statement = """CREATE TABLE IF NOT EXISTS students(
-        username VARCHAR PRIMARY KEY,
-        password VARCHAR NOT NULL,
-        studentno VARCHAR NOT NULL,
-        name VARCHAR NOT NULL,
-        surname VARCHAR NOT NULL
-        )"""
+url = "postgres://postgres@localhost:5432/postgres"
+initialize(url)
 
-url = "postgres://ffpzkcsbsmkffc:0bf6c8ea8127f14cb4da7d50542d9dadffa30fd97640dc6260cdac27f9762656@ec2-79-125-8-105.eu-west-1.compute.amazonaws.com:5432/d4280d6o5jiga1"
-connection = db.connect(url)
-cursor = connection.cursor()
-cursor.execute(statement)
-connection.commit()
-cursor.close()
-
-
-@app.route("/")
+@app.route('/home_page', methods=['GET', 'POST'])
 def home_page():
-    return render_template("home.html")
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        name = form.name.data
-        surname = form.surname.data
-        studentno = form.studentno.data
+    if request.method=="POST":
+        login = LoginForm(request.form["username"], request.form["password"])
         try:
             connection = db.connect(url)
             cursor = connection.cursor()
-            statement = "INSERT INTO students VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(statement, (username, password, studentno, name, surname))
-            result = cursor.fetchone()
-            flash('Your account was created successfully')
-            return redirect(url_for('login'))
+            statement = "SELECT * FROM student WHERE ((username = %s) AND (password = %s))"
+            cursor.execute(statement, (login.username, login.password))
+            cursor.close()
+            return redirect(url_for('profile'))
         except db.DatabaseError:
             connection.rollback()
             flash('Unsuccessful', 'danger')
         finally:
             connection.close()
-    return render_template('register.html', form=form)
+    return render_template("home.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method=="POST":
+        myform = RegistrationForm(request.form["studentno"], request.form["username"], request.form["password"], request.form["name"], request.form["surname"])
+        try:
+            connection = db.connect(url)
+            cursor = connection.cursor()
+            statement = "INSERT INTO student VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(statement, (myform.studentno, myform.username, myform.password, myform.name, myform.surname))
+            #result = cursor.fetchone()
+            cursor.close()
+            flash('Your account was created successfully')
+            return redirect(url_for('home_page'))
+        except db.DatabaseError:
+            connection.rollback()
+            flash('Unsuccessful', 'danger')
+        finally:
+            connection.close()
+    return render_template('register.html')
+
+@app.route("/profile")
+def profile():
+        connection = db.connect(url)
+        cursor = connection.cursor()
+        statement = "SELECT name FROM student"
+        cursor.execute(statement)
+        result = cursor.fetchall()
+        sname = result
+        for name in result:
+            print(name)
+            sname = name
+        return render_template("profile.html", name=sname)
 
 
 if __name__ == "__main__":
