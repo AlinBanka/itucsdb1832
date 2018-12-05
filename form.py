a3bcf0005b4b23dfb260544abd6b54d275518149
AlinBanka <alin.banka@gmail.com>
2018-12-05 14:06:11 +0300

    Matching my forms to the types used in server.py

--- form.py
+++ form.py
@@ -2,20 +2,16 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, validators
 
 
-class RegistrationForm(Form):
-    username = StringField('username', [validators.Length(min=4, max=25)])
-    name = StringField('name', [validators.Length(min=6, max=35)])
-    surname = StringField('surname', [validators.Length(min=6, max=35)])
-    password = PasswordField('new password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('retype password')
-    studentno = StringField('student number', [validators.Length(9)])
-    submit = SubmitField('CREATE')
+class RegistrationForm:
+    def __init__(self, studentno, username, password, name, surname):
+        self.studentno = studentno
+        self.username = username
+        self.password = password
+        self.name = name
+        self.surname = surname
+
     
-class LoginForm(Form):
-    username = StringField('username', [validators.DataRequired()])
-    password = PasswordField('password', [
-        validators.DataRequired()
-    ])
+class LoginForm:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
